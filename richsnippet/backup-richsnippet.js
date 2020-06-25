97109cf238bd07ada9b7ce2ccbc64b1455bf0583
Fahmi Alhafid <fahmyalhafidz@gmail.com>
2020-06-25 13:32:16 +0700

    Create backup-richsnippet.js
    Backup Rich Snippet (Structured Data fahmyalhafidz.blogspot.com) Web Name & Search Box

---
+++ richsnippet/backup-richsnippet.js
@@ -0,0 +1,42 @@
+<script type='application/ld+json'>
+{
+    &quot;@context&quot;: &quot;http://schema.org&quot;,
+    &quot;@type&quot;: &quot;Organization&quot;,
+    
+  &quot;name&quot;:&quot;Fahmy Alhafidz | Situs Pendidikan Gratis&quot;,
+  &quot;description&quot;:&quot;Fahmy Alhafidz | Situs Pendidikan Gratis yang menyediakan sumber sumber materi SMP, SMA, Kuliah, UN dan SBMPTN&quot;,
+  &quot;logo&quot;: &quot;https://1.bp.blogspot.com/-DeX1PSyYAV4/W78_xlfJvAI/AAAAAAAAAGU/WkQO0U4iJH0mTPjDiKpBsa0YCEjLRP6DgCK4BGAYYCw/s1600/fahmyalhafidz.png&quot;,
+  &quot;image&quot;:&quot;https://1.bp.blogspot.com/-DeX1PSyYAV4/W78_xlfJvAI/AAAAAAAAAGU/WkQO0U4iJH0mTPjDiKpBsa0YCEjLRP6DgCK4BGAYYCw/s1600/fahmyalhafidz.png&quot;,
+  &quot;url&quot;: &quot;https://fahmyalhafidz.blogspot.com/&quot;,
+  &quot;telephone&quot;: &quot;+6281222363681&quot;,
+  &quot;sameAS&quot;:[&quot;https://twitter.com/falhafizh&quot;], 
+  
+  &quot;contactPoint&quot;: [
+      {   
+    &quot;@type&quot;: &quot;ContactPoint&quot;,
+      &quot;telephone&quot;: &quot;+62 81 222 3636 81&quot;,
+      &quot;contactType&quot;: &quot;technical support&quot;,
+      &quot;contactOption&quot;: &quot;TollFree&quot;,
+      &quot;areaServed&quot;:&quot;Indonesia&quot;,
+      &quot;availableLanguage&quot;: [
+          &quot;English&quot;,
+          &quot;Indonesian&quot;
+    ]
+    }
+  ]
+}
+</script>
+
+
+<script type='application/ld+json'>
+{
+  &quot;@context&quot;: &quot;http://schema.org&quot;,
+  &quot;@type&quot;: &quot;WebSite&quot;,
+  &quot;url&quot;: &quot;https://fahmyalhafidz.blogspot.com/&quot;,
+  &quot;potentialAction&quot;: {
+    &quot;@type&quot;: &quot;SearchAction&quot;,
+    &quot;target&quot;: &quot;https://fahmyalhafidz.blogspot.com/search?q={search_term_string}&quot;,
+    &quot;query-input&quot;: &quot;required name=search_term_string&quot;
+  }
+}
+</script>	
